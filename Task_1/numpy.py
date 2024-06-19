025671b72c002af63fe11e2ac7b4562a0d380c54
ADEGOKE Ulrich <ulrichadegoke@gmail.com>
2024-06-19 13:08:50 +0100

    Array Additions

--- Task_1/numpy.py
+++ Task_1/numpy.py
@@ -13,8 +13,36 @@ class Array:
     def __repr__(self) -> str:
         return f"Array(elements={self.elements}, shape={self.shape})"
     
-
-D2_Array = Array([[1, 2], [3, 4], [5, 6], [7, 8]])
-print(D2_Array)
-print(D2_Array.elements)
-print(D2_Array.shape)  
+    def __add__(self, other):
+        if isinstance(other, Array):
+            if self.shape != other.shape:
+                raise ValueError("Arrays must have the same shape") 
+            result = [
+                [sum(pair) for pair in zip(row1, row2)]  
+                for row1, row2 in zip(self.elements, other.elements)
+            ] 
+            return Array(result)
+        elif isinstance(other, int) or isinstance(other, float):
+            if self.shape[0] == 1:
+                # 1D Array Case
+                result = [[x + other for x in self.elements[0]]]
+            else:
+                # 2D Array Case
+                result = [
+                    [x + other for x in row]
+                    for row in self.elements
+                ]
+            return Array(result)
+        else:
+            raise TypeError("Unsupported operand type(s) for +: 'Array' and '{}'".format(type(other)))
+       
+    # Commutative addition 
+    def __radd__(self, other):
+        return self.__add__(other)
+    
+D2_Array = Array([[1, 2], [3, 4]])
+D1_Array = Array([[1, 2], [3, 4]])
+D_Array = 5 + D2_Array
+print(D_Array)
+print(D_Array.elements)
+print(D_Array.shape)  
